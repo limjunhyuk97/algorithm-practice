b89d55b4500217c1ac8f84e9b7803d2d437d1dc2
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2020-12-11 17:33:25 +0900

    Update quicksort.cpp

--- Sort/quicksort/quicksort.cpp
+++ Sort/quicksort/quicksort.cpp
@@ -1,27 +1,20 @@
-void Qsort(int * array, int start, int end){
-	
-	if(start>=end) return;
-	
-	int pivot = start, i = start + 1, j = end - 1;
-	
-	while(i<=j){
-		while(i <= end && array[pivot] >= array[i]){
-			++i;
-		}
-		while(j > start && array[pivot] <= array[j] ){
-			--j;
-		}
-		if(i<j) swap(array[i], array[j]);
-		else swap(array[j], array[pivot]);
-	} 
-	
-	Qsort(array, start, j-1);
-	Qsort(array, j+1, end);
-	
+void quickSort(int A[], int low, int high) {
+    if(low >= high) return; // base condition
+
+    // divide process
+    int i = low-1, j = low;
+    int&pivot = A[high];
+    for (; j < high; ++j) {
+        if ( A[j] < pivot)
+            swap(A[++i], A[j]);
+    }
+    swap(A[++i], pivot);
+
+    // conquer process
+    quickSort(A, low, i-1);
+    quickSort(A, i+1, high);
 }
-inline void swap(int &n, int &m){
-	int tmp;
-	tmp = n;
-	n = m;
-	m= tmp;
+
+inline void swap(int &a, int &b){
+    int t = a; a = b; b = t;
 }
