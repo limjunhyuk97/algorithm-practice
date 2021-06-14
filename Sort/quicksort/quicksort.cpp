55d43c6e098bee924c6e8d4af85f43d854b9941b
Jun Hyuk Lim <limjunhyuk97@naver.com>
2021-06-14 17:33:54 +0900

    Update quicksort.cpp

--- Sort/quicksort/quicksort.cpp
+++ Sort/quicksort/quicksort.cpp
@@ -1,20 +1,62 @@
-void quickSort(int A[], int low, int high) {
-    if(low >= high) return; // base condition
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-    // divide process
-    int i = low-1, j = low;
-    int&pivot = A[high];
-    for (; j < high; ++j) {
-        if ( A[j] < pivot)
-            swap(A[++i], A[j]);
-    }
-    swap(A[++i], pivot);
-
-    // conquer process
-    quickSort(A, low, i-1);
-    quickSort(A, i+1, high);
+void swap(int* n, int* m) {
+	int tmp;
+	tmp = *n;
+	*n = *m;
+	*m = tmp;
 }
 
-inline void swap(int &a, int &b){
-    int t = a; a = b; b = t;
+// 중복된 값에 대한 정렬까지 고려
+void QuickSort(int arr[], int left, int right) {
+
+	if (left >= right)
+		return;
+
+	int start = left + 1;
+	int end = right;
+	int pivot = left;
+
+	while (start < end) {
+
+		while (arr[pivot] >= arr[start] && (start < right))
+			start++;
+
+		while (arr[pivot] <= arr[end] && (end > left))
+			--end;
+
+		if (start < end)
+			swap(&arr[start], &arr[end]);
+
+	}
+
+	swap(&arr[pivot], &arr[end]);
+
+	QuickSort(arr, left, end - 1);
+	QuickSort(arr, end + 1, right);
+
+}
+
+int main(void) {
+	
+	int arr[10];
+
+	srand((unsigned int)time(NULL));
+
+	for (int i = 0; i < 10; ++i) {
+		arr[i] = rand() % 10 + 1;
+		printf("%d ", arr[i]);
+	}
+	printf("\n\n");
+
+	QuickSort(arr, 0, 9);
+
+	for (int i = 0; i < 10; ++i) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n\n");
+
+	return 0;
 }
